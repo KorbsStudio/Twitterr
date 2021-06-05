b714921c79c35d548d8279e156c2569d42c7daf6
KorbsStudio <korbsstudio@github.com>
2021-06-04 20:03:01 -0400

    Add splash window

--- src/js/electron/index.js
+++ src/js/electron/index.js
@@ -2,6 +2,8 @@ const {app, BrowserWindow, ipcMain, Menu, shell, webContents} = require('electro
 const isMac = process.platform === 'darwin'
 const contextMenu = require('electron-context-menu');
 const { autoUpdater } = require('electron-updater');
+const path = require('path');
+const url = require('url');
 
 function createWindow () {
   const mainWindow = new BrowserWindow({
@@ -13,6 +15,7 @@ function createWindow () {
     frame: true,
     transparent: true,
     autoHideMenuBar: true,
+    show: false,
     webPreferences: {
       webviewTag: true,
       nodeIntegration: true,
@@ -21,10 +24,33 @@ function createWindow () {
     }
   })
   autoUpdater.checkForUpdatesAndNotify();
-  mainWindow.loadURL('https://twitter.com/twitter');
+  mainWindow.loadURL('https://twitter.com/');
   mainWindow.webContents.on('did-finish-load', function() {
     mainWindow.webContents.insertCSS('a.css-4rbku5.css-18t94o4.css-1dbjc4n.r-1habvwh.r-1loqt21.r-6koalj.r-eqz5dr.r-16y2uox.r-1ny4l3l.r-oyd9sg.r-13qz1uu[href="/i/bookmarks"]{display:none!important}a.css-4rbku5.css-18t94o4.css-1dbjc4n.r-1habvwh.r-1loqt21.r-6koalj.r-eqz5dr.r-16y2uox.r-1ny4l3l.r-oyd9sg.r-13qz1uu[aria-label=Lists]{display:none!important}.r-1kqtdi0{border-color:transparent!important}.css-1dbjc4n.r-1ysxnx4.r-1igl3o0.r-rull8r.r-qklmqi.r-2sztyj.r-1efd50x.r-5kkj8d.r-tbmifm{display:none!important}.r-1igl3o0{border-bottom-color:transparent!important}.r-2sztyj{border-top-color:transparent!important}.css-1dbjc4n.r-1ysxnx4.r-1igl3o0.r-rull8r.r-qklmqi.r-tbmifm{display:none!important}');
  })
+ const splash = new BrowserWindow({
+   width: 400,
+   height: 200,
+   darkTheme: true,
+   frame: false,
+   transparent: false,
+   minimizable: false,
+   maximizable: false,
+   autoHideMenuBar: true,
+   skipTaskbar: true,
+   webPreferences: {
+     devTools: true
+   }
+ })
+ splash.loadFile('file:///${__dirname}/../../src/html/splash.html');
+ setTimeout(() => { // Show splash for 5 seconds (fixed time) then the main window
+  mainWindow.show();
+ }, 3000); 
+ setTimeout(() => {
+  splash.close();
+ }, 3000);
+ splash.setIcon(path.join(__dirname, '../../images/icons/icon.png'));
+ mainWindow.setIcon(path.join(__dirname, '../../images/icons/icon.png'));
 }
 
 app.whenReady().then(() => {createWindow()})
